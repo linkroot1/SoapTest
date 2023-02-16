30b594c74a61fe0a3fd332d987e8f43c49422a4c
linkroot1 <lekha-now@mail.ru>
2023-02-16 09:45:10 +0300

    Init commit

---
+++ src/main/java/ru/pavlenko/project/SpringSoapApplication.java
@@ -0,0 +1,12 @@
+package ru.pavlenko.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringSoapApplication.class, args);
+    }
+}
